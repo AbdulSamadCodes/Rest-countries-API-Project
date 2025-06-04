88892593ed219ec5d05c3e38b7b5fb0a353a421c
Abdul Samad <samadcom797@gmail.com>
2025-06-04 16:15:31 +0500

    Fixed custom loader

--- src/Components/Dashboard/Loader.jsx
+++ src/Components/Dashboard/Loader.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-function Loader( {width } ) {
+function Loader( { width } ) {
+  const loaderWidth = `${width.toString()}rem`;
+  const loadingIndicatorWidth = `${(width / 10).toString()}rem`;
 
   return (
     <div className='loader mx-auto' 
-    style={{ 'width' : width.toString() + 'rem'}}>
+    style={{ 'width' : loaderWidth 
+           ,'borderWidth': loadingIndicatorWidth}}>
       
     </div>      
   )
--- src/Components/ImageWithLoader.jsx
+++ src/Components/ImageWithLoader.jsx
@@ -3,7 +3,10 @@ import { useState,useEffect,useRef } from 'react';
 import { Loader } from '/src/Components/Dashboard/Loader.jsx';
 
 function ImageWithLoader({ src }) {
-    
+  
+   return <Loader width={2.5}>
+
+   </Loader>   
 }
 
 export { ImageWithLoader };
